caacb0f6794bbcfda0eae56b9f300833fcbe4945
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2023-06-14 18:20:18 -0400

    Section 4 complete - added animation with framer-motion to feedback items on add or delete fade in fade out

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 import PropTypes from 'prop-types';
 
@@ -8,17 +9,40 @@ function FeedbackList({ feedback, handleDelete }) {
         return <p>No feedback yet</p>
     }
 
-  return (
-    <div className="feedback-list">
-        {feedback.map((item) => (
-            <FeedbackItem 
-                key={item.id} 
-                item={item} 
-                handleDelete={handleDelete}
-            />
-        ))}
-    </div>
-  )
+    // No animation on add feedback and delete
+//   return (
+//     <div className="feedback-list">
+//         {feedback.map((item) => (
+//             <FeedbackItem 
+//                 key={item.id} 
+//                 item={item} 
+//                 handleDelete={handleDelete}
+//             />
+//         ))}
+//     </div>
+//   )
+
+    // With animation
+    return (
+        <div className="feedback-list">
+            <AnimatePresence>
+                {feedback.map((item) => (
+                    <motion.div 
+                        key={item.id}
+                        initial={{opacity: 0}}
+                        animate={{opacity: 1}}
+                        exit={{opacity: 0}} 
+                    >
+                        <FeedbackItem 
+                            key={item.id} 
+                            item={item} 
+                            handleDelete={handleDelete}
+                        />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
+        </div>
+    )
 }
 
 FeedbackList.propTypes = {
