7dffbcae153dba463e559612dc9e650cc7d37d91
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2023-06-15 22:53:46 -0400

    Section 7 - created fetch requests for updating and deleting feedback

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -39,15 +39,35 @@ export const FeedbackProvider = ({ children }) => {
   }
 
   // Delete feedback item
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' })
+
         setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
   // Update feedback item
-  const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map((item) => item.id === id ? {...item, ...updItem} : item))
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem)
+    })
+
+    const data = await response.json()
+
+    // NOTE: no need to spread {...item, ...data} data and item
+    setFeedback(feedback.map((item) => item.id === id ? data : item))
+
+    // FIX: this fixes being able to add a feedback after editing
+    // credit to Jose https://www.udemy.com/course/react-front-to-back-2022/learn/lecture/29768200#questions/16462688
+    setFeedbackEdit({
+      item: {},
+      edit: false
+    })
   }
 
   // Set item to be updated
